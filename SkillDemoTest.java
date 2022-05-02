e3ed2ad98b56052f38d80be40dffb4c4af09ff6f
Lixin <alixintong@gmail.com>
2022-05-01 17:47:21 -0700

    add test

---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class SkillDemoTest {
+    @Test
+    public void multTest(){
+        assertEquals(6, SkillDemo.multiply(2,2));
+    }
+}
